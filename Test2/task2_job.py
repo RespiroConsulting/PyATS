2f0b1a626a5eb683ebd20a30b27adaa40ac3be4b
HRSallepalli95 <67352332+HRSallepalli95@users.noreply.github.com>
2021-01-12 15:26:20 +1100

    Create task2_job.py

---
+++ Test2/task2_job.py
@@ -0,0 +1,13 @@
+"""
+<PYATS_JOBFILE>
+"""
+# To run the job:
+# pyats run job example_job.py
+
+import os
+import time
+from pyats.easypy import run
+
+# All run() must be inside a main function
+def main():
+    run('task2_script.py')
