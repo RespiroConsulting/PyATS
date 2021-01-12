e5a9c24187109688d61dff624177c38510af267f
HRSallepalli95 <67352332+HRSallepalli95@users.noreply.github.com>
2021-01-12 15:34:44 +1100

    Create analysisreport.py

---
+++ Test3/analysisreport.py
@@ -0,0 +1,20 @@
+import json
+import requests
+
+def main():
+    f = open("out.txt").read()
+    print("""This test is performed to identify the changes in BGP network include,
+             1. Number of network entries
+             2. Number of BGP neighbors
+             3. BGP neighbors status
+             4. Number of BGP routes""")
+    if f == "":
+        print('\n'+"No changes found in BGP network before and after neighbor change"+'\n')
+        simplefile = open("doc1.txt").read()
+        print(simplefile)
+        print("Please find the attached file(s) for more detailed output of each command.")
+    else:
+        print('\n'+"Observed changes in BGP settings before and after neighbor change:"+'\n' +f)
+        print("Please find the attached file(s) for more detailed output of each command.")
+if __name__ == '__main__':
+    main()
